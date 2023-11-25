9228b6c29cab155fc1e5bd697237bb1077aca7b0
PutraRidhoIlahi <ilahiridhoo26@gmail.com>
2023-11-25 13:17:43 +0700

    mengisi 2 file di authocontroller dan homecontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class AuthController extends Controller
 {
-    //
+    public function register()
+    {
+        return view('register');
+    }
+    public function welcome(Request $request)
+    {
+        $data = [
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            // Add other form data as needed
+        ];
+
+        return view('welcome', $data);
+    }
 }
